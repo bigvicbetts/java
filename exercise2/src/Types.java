5ffcf42671b1cb97a601dc402c27bfaa9ba9592a
Victor Betts <v_betts22@hotmail.com>
2020-06-23 22:27:16 -0500

    Completed both exercises with bonuses for both (hopefully)

---
+++ exercise2/src/Types.java
@@ -0,0 +1,20 @@
+package exercise2;
+
+class Types {
+//    This class has syntax errors, that will prevent your other classes from running.  So... work on this file
+//    first to resolve the issues.
+
+    boolean number;
+    int[] myArray = {3, 4, 5, 6, 76, 12, 2};
+    String movieA = "Harry and the Hendersons";
+    String movieB = "Star Wars";
+    char male = 'm';
+    char female = 'f';
+
+//    The above instance variables have mistakes with their data types.  Fix the data types so the main method
+//    correctly displays the value of all of the variables
+
+    public static void main() {
+        System.out.println("There are variables that have been declared in this Accessible class.  Fix the code so that all the variables can be accessed by the main method.  print out the values of every variable once the code has been refactored.");
+    }
+}
--- udemy_exercises/song_player/src/Main.java
+++ udemy_exercises/song_player/src/Main.java
@@ -1,57 +1,126 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
 
     private static ArrayList<Album> myAlbums = new ArrayList<Album>();
     private static int playingSong = 0;
+    private static boolean quit = false;
 
     private static void playPlayList(LinkedList<Song> playList) {
-        if (playList.isEmpty()) {
-            System.out.println("You have no songs in your play list.");
+            if (playList.isEmpty()) {
+                System.out.println("You have no songs in your play list.");
+            }
+            else {
+                System.out.println("Now playing " + playList.get(playingSong).getName());
+            }
         }
-        else {
-            System.out.println("Now playing " + playList.get(playingSong).getName());
-        }
-    }
+
 
     private static void nextSong(LinkedList<Song> playList) {
-        playingSong++;
-        if (playList.size() > playingSong) {
-            System.out.println("Now playing " + playList.get(playingSong).getName());
+            playingSong++;
+            if (playList.size() > playingSong) {
+                System.out.println("Now playing " + playList.get(playingSong).getName());
+            }
+            else {
+                playingSong = playList.size();
+                System.out.println("You have reached the end of your play list.");
+            }
         }
-        else {
-            playingSong = playList.size();
-            System.out.println("You have reached the end of your play list.");
-        }
-    }
+
 
     private static void previousSong(LinkedList<Song> playList) {
-        playingSong--;
-        if (playingSong < 0) {
-            playingSong = 0;
-            System.out.println("You are at the beginning of your play list.");
+
+            playingSong--;
+            if (playingSong < 0) {
+                playingSong = 0;
+                System.out.println("You are at the beginning of your play list.");
+            }
+            else {
+                System.out.println("Now playing " + playList.get(playingSong).getName());
+            }
         }
-        else {
-            System.out.println("Now playing " + playList.get(playingSong).getName());
-        }
-    }
+
 
     private static void repeatSong(LinkedList<Song> playList) {
-        System.out.println("Now playing " + playList.get(playingSong).getName());
-    }
+
+            System.out.println("Now playing " + playList.get(playingSong).getName());
+        }
+
+
 
     private static void showPlayList(LinkedList<Song> playList) {
-        for (int i = 0; i < playList.size(); i++) {
-            System.out.println(i + 1 + ": " + playList.get(i).getName());
+
+            for (int i = 0; i < playList.size(); i++) {
+                System.out.println(i + 1 + ": " + playList.get(i).getName());
+            }
         }
-    }
+
+
 
     private static void removeSong(String title, LinkedList<Song> playList) {
-        for (int i = 0; i < playList.size(); i++) {
-            if (playList.get(i).getName().equals(title)) {
-                playList.remove(i);
+
+            for (int i = 0; i < playList.size(); i++) {
+                if (playList.get(i).getName().equals(title)) {
+                    playList.remove(i);
+                }
             }
+        }
+
+
+
+    private static void quit() {
+        quit = true;
+    }
+
+    private static void menu(LinkedList<Song> playList) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Main Menu");
+        System.out.println("1. Play play list");
+        System.out.println("2. Play previous song");
+        System.out.println("3. Play next song");
+        System.out.println("4. Repeat current song");
+        System.out.println("5. Remove song");
+        System.out.println("6. Show play list");
+        System.out.println("7. Quit");
+        System.out.println("===============");
+        System.out.print("Enter selection: ");
+        choice = scanner.nextInt();
+
+        switch(choice) {
+            case 1:
+                playPlayList(playList);
+                System.out.println("\n\n");
+                break;
+            case 2:
+                previousSong(playList);
+                System.out.println("\n\n");
+                break;
+            case 3:
+                nextSong(playList);
+                System.out.println("\n\n");
+                break;
+            case 4:
+                repeatSong(playList);
+                System.out.println("\n\n");
+                break;
+            case 5:
+                String title;
+                System.out.println("Enter song title to remove: ");
+                title = scanner.nextLine();
+                removeSong(title, playList);
+                System.out.println("\n\n");
+                break;
+            case 6:
+                showPlayList(playList);
+                System.out.println("\n\n");
+                break;
+            case 7:
+                quit();
+                break;
         }
     }
 
@@ -94,6 +163,8 @@ public class Main {
         myAlbums.get(1).addToPlaylist("My Song 2g", myPlayList);
         myAlbums.get(1).addToPlaylist("My Song 2f", myPlayList);
 
+        /*
+        turnOn();
         showPlayList(myPlayList);
         playPlayList(myPlayList);
         nextSong(myPlayList);
@@ -103,6 +174,10 @@ public class Main {
         previousSong(myPlayList);
         removeSong("My Song 1d", myPlayList);
         showPlayList(myPlayList);
+        */
 
+        do {
+            menu(myPlayList);
+        } while (!quit);
     }
 }
